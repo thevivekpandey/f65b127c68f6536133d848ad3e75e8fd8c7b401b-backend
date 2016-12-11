52cb76e0e42eb783067ca216e0ce566aa05d5447
thevivekpandey <thevivekpandey@gmail.com>
2016-12-11 22:58:20 +0530

    Attempting to user headers

--- server.js
+++ server.js
@@ -6,20 +6,30 @@ const PORT = 8000;
 var app  = express();
 
 app.get("/run", function(req, res) {
-  console.log("Method = " + req.query.method);
-  console.log("URL = " + req.query.url);
   url = req.query.url;
   method = req.query.method;
+  key = req.headers.headerskey.split("|||")[0];
+  val = req.headers.headerskey.split("|||")[1];
+  console.log(url);
+  console.log(method);
+  console.log(key);
+  console.log(val);
   axios({
     method: method,
-    url: url
+    url: url,
+    headers: {key: val}
   }).then(function(response) {
     res.header("Access-Control-Allow-Origin", "*");
+    console.log(response.data);
     res.send(response.data);
   }).catch(function(error) {
-    console.log(error);
     res.send(error);
   })
 });
 
+app.options("/run", function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "headerskey");
+  res.send();
+});
 app.listen(PORT);
