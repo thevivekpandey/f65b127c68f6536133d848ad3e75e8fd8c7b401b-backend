257ecbb9a847907d9f92568cbb1f4df060322543
thevivekpandey <thevivekpandey@gmail.com>
2016-12-17 20:49:46 +0530

    Backend takes any method. Does not work currently

--- server.js
+++ server.js
@@ -7,32 +7,26 @@ var app  = express();
 
 app.get("/run", function(req, res) {
   url = req.query.url;
-  method = req.query.method;
+  method = req.query.method.toLowerCase();
+  console.log(url);
+  console.log(method);
   headers = {};
   if ("headerskey" in req.headers) {
     key = req.headers.headerskey.split("|||")[0];
     val = req.headers.headerskey.split("|||")[1];
-    console.log(url);
-    console.log(method);
-    console.log(req.headers);
-    console.log(key);
-    console.log(val);
     headers[key] = val;
   }
-
   axios({
-    method: method,
     url: url,
+    method: method,
     headers: headers
   }).then(function(response) {
+    console.log(response);
     res.header("Access-Control-Allow-Origin", "*");
-    console.log(response.data);
     res.send(response.data);
   }).catch(function(error) {
-    console.log(error);
-    console.log("This was the error");
     res.send(error);
-  })
+  });
 });
 
 app.options("/run", function(req, res) {
