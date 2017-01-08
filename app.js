a7a55bdd81bb94ac84e51111ce6adf5df0975ba2
thevivekpandey <thevivekpandey@gmail.com>
2017-01-08 18:50:07 +0530

    return interval

--- app.js
+++ app.js
@@ -5,31 +5,30 @@ const PORT = 8000;
 
 var app  = express();
 
-send = function(res, data) {
+send = function(res, data, startTime) {
   res.header("Access-Control-Allow-Origin", "*");
+  interval = new Date() - startTime;
+  data.interval = interval;
   res.send(data);
 }
 app.get("/run", function(req, res) {
   url = req.query.url;
   method = req.query.method.toLowerCase();
-  console.log(url);
-  console.log(method);
   headers = {};
   if ("headerskey" in req.headers) {
     key = req.headers.headerskey.split("|||")[0];
     val = req.headers.headerskey.split("|||")[1];
     headers[key] = val;
   }
+  startTime = new Date();
   axios({
     url: url,
     method: method,
     headers: headers
   }).then(function(response) {
-    console.log(response);
-    send(res, {data: response.data, headers: response.headers, status: response.status});
+    send(res, {data: response.data, headers: response.headers, status: response.status}, startTime);
   }).catch(function(error) {
-    console.log(error);
-    send(res, {data: error.response.data, status: error.response.status});
+    send(res, {data: error.response.data, status: error.response.status}, startTime);
   });
 });
 
